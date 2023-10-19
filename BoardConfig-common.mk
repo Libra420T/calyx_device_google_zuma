ZeNiXxX/android_device_google_gs101

Create BoardConfig-calyx
Change-Id: I8136e9f0df40ca3bf96dbfd2453275481178cdaa

BoardConfig-common.mk
@@ -418,3 +418,5 @@ BOARD_KERNEL_CMDLINE += at24.write_timeout=100
 BOARD_KERNEL_CMDLINE += log_buf_len=1024K
 
 -include vendor/google_devices/gs101/proprietary/BoardConfigVendor.mk
+
+include device/google/gs101/BoardConfig-calyx.mk

Libra420T/calyx_device_google_zuma

BoardConfig-common.mk
@@ -423,3 +423,5 @@ BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 
 # pick up library for cleaning digital car keys on factory reset
 -include vendor/google_devices/gs-common/proprietary/BoardConfigVendor.mk
+
+include device/google/zuma/BoardConfig-calyx.mk