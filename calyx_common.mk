dc9722c95cef58663bd0c5141c01b3525dfb93f6
LuK1337 <priv.luk@gmail.com>
2023-12-06 09:54:05 +0100

    Add liboemcrypto.so to vendor linker config
    This lets com.google.android.widevine.apex access it.
    
    Change-Id: I4e9654fe6317b722f5fbf43b92abb367c18640ce

--- calyx_common.mk
+++ calyx_common.mk
@@ -11,3 +11,7 @@ DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-calyx
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+# Linker config
+PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
+    device/google/zuma/linker.config.json
